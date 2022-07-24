4286fede78c2f581aaa7068d3775cb47c2d7f96c
Michelle Morilhas <michelle.morilhas@gmail.com>
2022-07-24 19:27:04 -0300

    criação de mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import dio.desafio.dominio.Curso;
+import dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso("curso js", "descrição curso js", 6);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
